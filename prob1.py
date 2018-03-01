23640a4fd1c942c871abc87189a57aec312e7841
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-01 17:29:36 +0530

    Added some comments

--- prob1.py
+++ prob1.py
@@ -1,12 +1,19 @@
 '''
+Python Assignment 1 Reboot Academy 
 Sales Price Question 1 
-Solved by Kamesh Kotwani
+Solved by: Kamesh Kotwani
 '''
 
+#To input the user name 
 name = input("Please Enter your name : ")
 print(f"Hello {name}, Welcome to sales price!")
-print("Please Enter the Total amount")
-amount = input()
-print("Calculating amount after 20% discount")
+
+#To take the input from user for the amount
+amount = input("Please Enter the total amount in Rupees : ")
+print("Calculating amount after 20% discount.......")
+
+#To store the discounted price into another variable
 dis =float(amount)*0.80
-print(f"The Total amount you have to pay after 20% discount is : {dis}")
+
+#To print the discounted price
+print(f"The Total amount you have to pay after 20% discount is : {dis} Rupees")
