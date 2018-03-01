e02ec6f30fb497f61a803d9142fc8df519b4bbec
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-01 17:23:33 +0530

    Completed Question 1

---
+++ prob1.py
@@ -0,0 +1,12 @@
+'''
+Sales Price Question 1 
+Solved by Kamesh Kotwani
+'''
+
+name = input("Please Enter your name : ")
+print(f"Hello {name}, Welcome to sales price!")
+print("Please Enter the Total amount")
+amount = input()
+print("Calculating amount after 20% discount")
+dis =float(amount)*0.80
+print(f"The Total amount you have to pay after 20% discount is : {dis}")
