0f71f439f118210601305a69c190b9c4ffd8e5d5
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-09 21:50:59 +0530

    Half done series

---
+++ Assignment_1/fibonacci.py
@@ -0,0 +1,13 @@
+'''
+Fibonacci Series : Reboot Academy
+
+This solution is created in python 3.6.4
+CAUTION: MAY NOT WORK IN OLDER VERSION
+
+Solved by: Kamesh Kotwani
+'''
+print("****Welcome to Fibonacci Series****")
+n = int(input("Please enter until how many terms you want to print : "))
+while(n<0):
+    n = int(input("Please enter until how many terms you want to print : "))
+    
