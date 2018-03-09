9a04527353f15c309493155e176ca87dd11d22b2
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-09 18:56:56 +0530

    Completed but not checked

--- Assignment_1/prob3.py
+++ Assignment_1/prob3.py
@@ -9,3 +9,23 @@ Solved by: Kamesh Kotwani
 '''
 
 print("Welcome to Ingredients Counter!!")
+
+#Given quantity for 48 cookies
+cup_s = 1.5/48
+cup_b = 1/48
+cup_f = 2.75/48
+
+#To ask the user for number of cookies required
+num = int(input("Please enter the number of Cookies required to make : "))
+print("Calculating the ingerdients required.........")
+
+#Calculating the ingredients needed
+cups_r = cup_s*num
+cupb_r = cup_b*num
+cupf_r = cup_f*num
+
+#Displaying the number of cups needed for cookies entered by user
+print(f"You need {cups_r} of Sugar")
+print(f"You need {cupb_r} of Butter")
+print(f"You need {cupf_r} of Flour to make {num} cookies")
+
