d961955febe42d12a3bbe8e352e3f624055b6dff
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-09 18:31:16 +0530

    Added a condition for profit or loss

--- Assignment_1/prob4.py
+++ Assignment_1/prob4.py
@@ -14,21 +14,26 @@ print("Welcome to Stock Transction!")
 pur0 = 2000                    # No of shares Joe purchased
 price_one_share = 40.00        # Price of one share
 t_pur = pur0*price_one_share   # Total amount of money paid
-comm0 = t_pur*0.03              # Commision paid after first purchase
+comm0 = t_pur*0.03              # Commision paid after first purchase    
 
 #Selling the Stocks
 sell0 = 2000                        # No of shares Joe sold
 price_one_share = 42.75             # price of one share
 t_recieved = sell0*price_one_share  # total amount recieved
 comm1 = t_recieved*0.03             # Commision paid after selling
-
+#money_left_s = t_recieved-comm1
 commision = comm0+comm1             #Total money paid to broker
 
-profit = t_recieved-commision
+moneyleft = t_recieved-commision
 
 # Displaying information
 print(f"The amount of money Joe paid for the stock is  : $ {t_pur}")
 print(f"The amount of commision paid when bought stock : $ {comm0}")
 print(f"The amount that Joe got after selling stocks : $ {t_recieved}")
 print(f"The amount of money commision Joe paid when he sold the stock : $ {comm1}")
+print(f"The amount of money Joe has left is : $ {moneyleft}")
+if(moneyleft>0):
+    print(f"Joe has made a profit of :$ {moneyleft}")
+else:
+    print("Joe is in loss.")
 
