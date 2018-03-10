a99cb9b5a40463c3842d1dba79680ca6ef0a0b0d
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-10 10:16:09 +0530

    Under construction

--- Assignment_1/prob6.py
+++ Assignment_1/prob6.py
@@ -1,6 +1,6 @@
 '''
-Assignment_1:Question_3 Reboot Academy
-Cookies Program
+Assignment_1:Question_6 Reboot Academy
+
 
 This solution is created in python 3.6.4
 CAUTION: MAY NOT WORK IN OLDER VERSION
