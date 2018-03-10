c1a9949311007e37ffa3459ae6a1c0bef96b8f34
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-10 18:01:28 +0530

    Not able to do

---
+++ Assignment_1/prob7.py
@@ -0,0 +1,14 @@
+'''
+Python Assignment 1 Reboot Academy 
+Stair Step Question
+
+Created using Python 3.6.4 
+CAUTION: MAY NOT WORK IN OLDER 
+
+Solved by: Kamesh Kotwani
+'''
+for i in range(1, 5):
+    for j in range(i):
+        print(" ")
+       
+    print("#", end=" ")   
