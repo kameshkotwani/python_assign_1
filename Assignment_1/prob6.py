90f2cc08cf1a02664f1f2c9f813f9560f3a30b1c
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-10 22:01:33 +0530

    Half done

--- Assignment_1/prob6.py
+++ Assignment_1/prob6.py
@@ -1,9 +1,21 @@
 '''
 Assignment_1:Question_6 Reboot Academy
-
+Time Calculator
 
 This solution is created in python 3.6.4
 CAUTION: MAY NOT WORK IN OLDER VERSION
 
 Solved by: Kamesh Kotwani
 '''
+
+print("****Welcome to Time Calculator****")
+time = int(input("Please Enter number of seconds : "))
+while(time < 0):
+    time = int(input("Please Enter number of seconds : "))
+if(time >= 3600):
+    hours = time/3600
+    
+    print(f"Number of hours in {time} seconds is : {round(hours)} hours.")
+if(time<3600):
+    minutes = time / 60
+    print(f"{time} seconds have approx : {round(minutes)} minutes.")
