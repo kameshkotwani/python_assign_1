cfc3da4d21800c4f2481656876cc8c56930bb121
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-11 16:37:05 +0530

    Made changes and added comments

--- Assignment_1/prob6.py
+++ Assignment_1/prob6.py
@@ -12,10 +12,24 @@ print("****Welcome to Time Calculator****")
 time = int(input("Please Enter number of seconds : "))
 while(time < 0):
     time = int(input("Please Enter number of seconds [time cannot be negative] : "))
+
+#To count number of days 
 days = time // 86400
-time%=86400
-hours = time//3600
+
+#To seperate the remaining hours
+time%=86400             
+
+#To count number of hours
+hours = time//3600      
+
+#To seperate out remaining minutes
 time%=3600
+
+#To count number of minutes
 minutes = time // 60
+
+#Finally to seperate remaining seconds
 seconds = time%60
+
+#Printing values
 print(f"{time} seconds has approx: {days} days, {hours} hours, {minutes} minutes and {seconds} seconds.")
