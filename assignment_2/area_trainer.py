23be6ea8bc7863cdda55e8876a01f9734918f998
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-15 16:10:18 +0530

    Optimized Checked Function and implemented SCORE COUNTER

--- assignment_2/area_trainer.py
+++ assignment_2/area_trainer.py
@@ -11,16 +11,30 @@ Solved by: Kamesh Kotwani
 import math
 import random
 
+SCORE =0   #Global Score Variable
+
+#Function to keep track of score
+def increment():
+    global SCORE
+    SCORE+=1
+
 #A check Function to see if user has given correct answer
 def check(choice):
-    if(choice == 1):
-        print(" \n__You are correct, +1 point__\n")
+    if(choice == 1 or choice ==2 or choice ==3 or choice ==4):
+        if(choice == 1):
+            print(" \n__You are correct, +1 point__\n")
+            increment()  #increment score if the choice is correct
+        else:
+            print("\n__WRONG__\n")
     else:
-        print("\n__WRONG__\n")
+        choice = int(input("Please Enter 1 or 2 or 3 or 4 \nYOUR CHOICE : "))
+        
+        #Recursively Calling check function 
+        check(choice)
 
-#Defining an options area
+#Defining Options function to print the options
 def options(area):
-    print("Only one of them is correct, Please enter your option")
+    print("\nOnly one of them is correct, Please enter your option")
     print("Option 1 : ", round(area,4))
     print("Option 2 : ", round(area+4,4))
     print("Option 3 : ", round(area+6,4))
@@ -28,41 +42,41 @@ def options(area):
 
 #Defining the Function for circle
 def circle():
-    #Using Random function to stimulate integers from 1 to 50
-    radius = random.randint(1, 20)
+    #Using Random function to stimulate integers from 1 to 20
+    radius = random.randint(1, 21)
     area = math.pi*(radius**2)
     #Printing Options
     print(f"Radius is  : {radius}")
     options(area)
     #Asking the user to input his/her choice
-    choice = int(input("Please Enter 1 or 2 or 3 or 4 : \nYOUR CHOICE : "))
+    choice = int(input("Please Enter 1 or 2 or 3 or 4 \nYOUR CHOICE : "))
     #Calling check function to see if user has input a correct option
     check(choice)
 
 #Defining rectangle Function
 def rectangle():
-    #Using Random function to stimulate integers from 1 to 50
-    length = random.randint(1, 20)
-    breadth = random.randint(1, 20)
+    #Using Random function to stimulate integers from 1 to 20
+    length = random.randint(1, 21)
+    breadth = random.randint(1, 21)
     area = length*breadth
     #Printing Options
     print(f"length is :  {length}, breadth is : {breadth}")
     options(area)
     #Asking the user to input his/her choice
-    choice = int(input("Please Enter 1 or 2 or 3 or 4 : "))
+    choice = int(input("Please Enter 1 or 2 or 3 or 4 \nYOUR CHOICE : "))
     #Calling check function to see if user has input a correct option
     check(choice)
 
 def triangle():
-    #Using Random function to stimulate integers from 1 to 50
-    height = random.randint(1, 20)
-    base = random.randint(1, 20)
+    #Using Random function to stimulate integers from 1 to 20
+    height = random.randint(1, 21)
+    base = random.randint(1, 21)
     area = 0.5*base*height
     #Printing Options
     print(f"height is :  {height}, base is : {base}")
     options(area)
     #Asking the user to input his/her choice
-    choice = int(input("Please Enter 1 or 2 or 3 or 4 : "))
+    choice = int(input("Please Enter 1 or 2 or 3 or 4 \nYOUR CHOICE : "))
     #Calling check function to see if user has input a correct option
     check(choice)
 
@@ -88,18 +102,17 @@ def main():
             rectangle()            
         
         elif(ch == 4):
-            print(f"Your current score is : __Under Construction__ ")
+            print(f"Your current score is : {SCORE} ")
         
         #Calling exit function to exit from program
         elif(ch ==5):
-            print("Goodbye,thanks for training! Have a nice day.")
-            print(f"Your Final score is : __Under Construction__ ")
+            print("\nGoodbye,thanks for training! Have a nice day.")
+            print(f"Your Final score is : {SCORE} \n")
             exit()       
             
         #If user inputs something else
         else:
             print("__Please select something else__ \n")
-            
 
 
 #Calling main function
