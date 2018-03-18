ddbeca1546b377eb76c57f563a1e9f09435402c8
Kamesh <kameshkotwanics2016@ggct.co.in>
2018-03-18 11:11:26 +0530

    Made some progress

--- assignment_3/password.py
+++ assignment_3/password.py
@@ -12,16 +12,6 @@ import string
 
 POINTS = 0
 
-#Increment fucntion to plus the score
-def increment():
-    global POINTS
-    POINTS += 1
-
-#decrement fucntion to deduct the score
-def decrement():
-    global POINTS
-    POINTS -= 1
-
 #Making a input check function, prompts user until he/she gives required input
 def input_check():
     con = True
@@ -32,11 +22,11 @@ def input_check():
             inp =str(input("Please Enter your Choice : "))
             con = False
             return inp
-        except :
+        except:
             print("Wrong Input Please input again")
         
 
-
+#Defining Options Function 
 def options():
     print("Press a to Check strength of your password")
     print("Press b to create a strong password for you")
@@ -47,9 +37,30 @@ def options():
 def passkey_checker(user_passkey):
     global POINTS
     POINTS = user_passkey.__len__()
+    if(POINTS < 8 or POINTS > 20):
+        user_passkey = str(input("Please Enter your password again 8-20 characters : "))
+        passkey_checker(user_passkey)
+    print("initial POINTS is now",POINTS)
+    for c in user_passkey:
+        if(c.islower()):
+            POINTS += 5
+            print("after islower POINTS is now",POINTS)
+            break
+    for c in user_passkey:
+        if(c.isupper()):
+            POINTS += 5
+            print("after isupper POINTS is now",POINTS)
+            break
+    for c in user_passkey:
+        if(c.isdigit()):
+            POINTS += 5
+            print("after is digit POINTS is now",POINTS)
+            break
+
 
 #Main function of the program
 def main():
+    global POINTS
     while(True):
         options()
         ch = (input("YOUR CHOICE : "))
@@ -57,11 +68,15 @@ def main():
         if(ch == chr(97)):
             user_passkey = str(input("Enter your password : "))
             passkey_checker(user_passkey)
-            print(user_passkey)
 
+        elif(ch == chr(98)):
+            print("__UNDER CONSTRUCTION__")
+        
         elif(ch == chr(99)):
             print("bye.")
             exit()
+        else:
+            print("ERROR.")
 
 
 if(__name__) == "__main__":
